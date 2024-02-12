2050b7bff2a0b513dfd5e7e0e0667cff8404d995
njaramoses <moses.njara@attain-es.com>
2024-02-13 00:07:05 +0300

    Persist and retrieve relational data by using Entity Framework Core

--- ContosoPizza/Program.cs
+++ ContosoPizza/Program.cs
@@ -1,5 +1,6 @@
 using ContosoPizza.Services;
 // Additional using declarations
+using ContosoPizza.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // Add the PizzaContext
+builder.Services.AddSqlite<PizzaContext>("Data Source=ContosoPizza.db");
 
 // Add the PromotionsContext
 
@@ -27,6 +29,7 @@ app.UseAuthorization();
 app.MapControllers();
 
 // Add the CreateDbIfNotExists method call
+app.CreateDbIfNotExists();
 
 app.MapGet("/", () => @"Contoso Pizza management API. Navigate to /swagger to open the Swagger test UI.");
 
